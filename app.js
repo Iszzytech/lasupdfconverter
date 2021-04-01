140fe3bfb2161ad13132d3322b02b3d39d20c03a
Iszzytech <iszzytechofficial@gmail.com>
2021-04-01 07:27:22 +0100

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 // imports
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');   
 const libre = require('libreoffice-convert');
 const fs = require("fs");
 const path = require("path");
 var outputFilePath;
-const multer = require('multer')
-const app = express() 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const multer = require('multer');
+const app = express(); 
 
 const PORT = process.env.PORT || 5000;
     
@@ -28,17 +24,19 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 //storage
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
-    },
-  });
+const storage = multer.diskStorage({
+destination: './public/uploads/',
+filename: function(req, file, cb){
+  cb(null,file.fieldname + '-' + Date.now() +
+  path.extname(file.originalname));
+}
+});
+
+// Init Upload
+const upload = multer({
+storage: storage,
+limits:{fileSize: 10}
+}).single('myFile');
 
 
 
@@ -63,62 +61,24 @@ app.get('/docxtopdfdemo', (req, res) => {
 })
 
 
-const docxtopdfdemo = function (req, file, callback) {
-    var ext = path.extname(file.originalname);
-    if (
-      ext !== ".docx" &&
-      ext !== ".doc"
-    ) {
-      return callback("This Extension is not supported");
-    }
-    callback(null, true);
-  };
-
-  const docxtopdfdemoupload = multer({storage:storage,fileFilter:docxtopdfdemo})
-
-  app.post('/docxtopdfdemo',docxtopdfdemoupload.single('file'),(req,res) => {
-    if(req.file){
-      console.log(req.file.path)
-  
-      const file = fs.readFileSync(req.file.path);
-
-      outputFilePath = Date.now() + "output.pdf" 
-  
-      libre.convert(file,".pdf",undefined,(err,done) => {
-        if(err){
-          fs.unlinkSync(req.file.path)
-          fs.unlinkSync(outputFilePath)
-  
-          res.send("some error taken place in conversion process")
-        }
-  
-        fs.writeFileSync(outputFilePath, done);
-  
-        res.download(outputFilePath,(err) => {
-          if(err){
-            fs.unlinkSync(req.file.path)
-          fs.unlinkSync(outputFilePath)
-  
-          res.send("some error taken place in downloading the file")
-          }
-  
-          fs.unlinkSync(req.file.path)
-          fs.unlinkSync(outputFilePath)
-        })
-  
-  
-      })
-    }
-  })
-
 
 
 app.get('/index', (req, res) => {
     res.render('index')
 })
-// app.get('/docxtopdfdemo',(req,res) => {
-//     res.render('docxtopdfdemo',{title: "DOCX to PDF converter"})
-// })
+
+app.post('/upload', (req, res) => {
+upload(req, res, (err) => {
+if (err){
+  res.render ('docxtopdfdemo', {
+    msg: err
+  });
+} else{
+  console.log(req.file);
+  res.send('test');
+}
+});
+});
 
 // listen on the port
 app.listen(PORT, () => {
